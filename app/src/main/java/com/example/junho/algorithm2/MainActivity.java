e001c6c3e812af03d78bd6bd96c0dbb86cb588da
philoz3231 <soaring3231@gmail.com>
2017-04-10 11:41:48 +0900

    update layout and class

--- app/src/main/java/com/example/junho/algorithm2/MainActivity.java
+++ app/src/main/java/com/example/junho/algorithm2/MainActivity.java
@@ -24,70 +24,139 @@ public class MainActivity extends AppCompatActivity {
         Button merge_btn = (Button)findViewById(R.id.button10);
         Button heap_btn = (Button)findViewById(R.id.button9);
         Button quick_btn = (Button)findViewById(R.id.button2);
-        final TextView result_textView = (TextView)findViewById(R.id.result_textView);
+        final TextView result_textView1 = (TextView)findViewById(R.id.result_textView1);
+        final TextView result_textView2 = (TextView)findViewById(R.id.result_textView2);
+        final TextView input_textView = (TextView)findViewById(R.id.editText3);
 
         compare_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Compare Clicked", Toast.LENGTH_SHORT).show();
-                result_textView.setText("Compare Button clicked\nBubble\nInsert\nselect\nEnd");
+                try{
+                    long seed = System.nanoTime();
+                    PackofCards cards1 = new PackofCards(Integer.parseInt(input_textView.getText().toString()), seed);
+                    long time1a = System.currentTimeMillis();
+                    cards1.BubbleSort();
+                    long time1b = System.currentTimeMillis();
+
+                    PackofCards cards2 = new PackofCards(Integer.parseInt(input_textView.getText().toString()), seed);
+                    long time2a = System.currentTimeMillis();
+                    cards2.InsertSort();
+                    long time2b = System.currentTimeMillis();
+
+                    PackofCards cards3 = new PackofCards(Integer.parseInt(input_textView.getText().toString()), seed);
+                    long time3a = System.currentTimeMillis();
+                    cards3.SelectSort();
+                    long time3b = System.currentTimeMillis();
+
+                    PackofCards cards4 = new PackofCards(Integer.parseInt(input_textView.getText().toString()), seed);
+                    long time4a = System.currentTimeMillis();
+                    cards4.MergeSort();
+                    long time4b = System.currentTimeMillis();
+
+                    PackofCards cards5 = new PackofCards(Integer.parseInt(input_textView.getText().toString()), seed);
+                    long time5a = System.currentTimeMillis();
+                    cards5.HeapSort();
+                    long time5b = System.currentTimeMillis();
+
+                    PackofCards cards6 = new PackofCards(Integer.parseInt(input_textView.getText().toString()), seed);
+                    long time6a = System.currentTimeMillis();
+                    cards6.QuickSort();
+                    long time6b = System.currentTimeMillis();
+
+                    result_textView1.setText("Your input: " + cards1.length + "\n"
+                        + "Seed: " + seed + "\n\n"
+                            + "Bubble: " + (time1b - time1a)/1000 + " sec\n"
+                            + "Insertion: " + (time2b - time2a)/1000 + " sec\n"
+                            + "Selection: " + (time3b - time3a)/1000 + " sec\n"
+                            + "Merge: " + (time4b - time4a)/1000 + " sec\n"
+                            + "Heap: " + (time5b - time5a)/1000 + " sec\n"
+                            + "Quick: " + (time6b - time6a)/1000 + " sec\n"
+                    );
+                } catch (Exception e){}
+
+
+
+                //Toast.makeText(getApplicationContext(), "Compare Clicked", Toast.LENGTH_SHORT).show();
+                //result_textView.setText("Compare Button clicked\nBubble\nInsert\nselect\nEnd");
             }
         });
         bubble_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Bubble Clicked", Toast.LENGTH_SHORT).show();
-                result_textView.setText("Bubble Button clicked");
+                try{
+                    PackofCards cards = new PackofCards((Integer.parseInt(input_textView.getText().toString())));
+                    result_textView1.setText("Your input: " + cards.length + "\n" + cards.PrintCards());
+                    long time1 = System.currentTimeMillis();
+                    cards.BubbleSort();
+                    long time2 = System.currentTimeMillis();
+                    result_textView2.setText("Bubble: " + (time2-time1)/1000. + "sec\n" + cards.PrintCards());
+                } catch(Exception e){
+                }
             }
         });
         insert_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Insert Clicked", Toast.LENGTH_SHORT).show();
+                try{
+                    PackofCards cards = new PackofCards((Integer.parseInt(input_textView.getText().toString())));
+                    result_textView1.setText("Your input: " + cards.length + "\n" + cards.PrintCards());
+                    long time1 = System.currentTimeMillis();
+                    cards.InsertSort();
+                    long time2 = System.currentTimeMillis();
+                    result_textView2.setText("Insertion: " + (time2-time1)/1000. + "sec\n" + cards.PrintCards());
+                } catch(Exception e){
+                }
             }
         });
         select_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Select Clicked", Toast.LENGTH_SHORT).show();
+                try{
+                    PackofCards cards = new PackofCards((Integer.parseInt(input_textView.getText().toString())));
+                    result_textView1.setText("Your input: " + cards.length + "\n" + cards.PrintCards());
+                    long time1 = System.currentTimeMillis();
+                    cards.SelectSort();
+                    long time2 = System.currentTimeMillis();
+                    result_textView2.setText("Selection: " + (time2-time1)/1000. + "sec\n" + cards.PrintCards());
+                } catch(Exception e){
+                }
             }
         });
         merge_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Merge Clicked", Toast.LENGTH_SHORT).show();
+                try{
+                    PackofCards cards = new PackofCards((Integer.parseInt(input_textView.getText().toString())));
+                    result_textView1.setText("Your input: " + cards.length + "\n" + cards.PrintCards());
+                    long time1 = System.currentTimeMillis();
+                    cards.MergeSort();
+                    long time2 = System.currentTimeMillis();
+                    result_textView2.setText("Merge: " + (time2-time1)/1000. + "sec\n" + cards.PrintCards());
+                } catch(Exception e){
+                }
             }
         });
         heap_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Heap Clicked", Toast.LENGTH_SHORT).show();
+                try{
+                    PackofCards cards = new PackofCards((Integer.parseInt(input_textView.getText().toString())));
+                    result_textView1.setText("Your input: " + cards.length + "\n" + cards.PrintCards());
+                    long time1 = System.currentTimeMillis();
+                    cards.HeapSort();
+                    long time2 = System.currentTimeMillis();
+                    result_textView2.setText("Heap: " + (time2-time1)/1000. + "sec\n" + cards.PrintCards());
+                } catch(Exception e){
+                }
             }
         });
         quick_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Quick Clicked", Toast.LENGTH_SHORT).show();
+                try{
+                    PackofCards cards = new PackofCards((Integer.parseInt(input_textView.getText().toString())));
+                    result_textView1.setText("Your input: " + cards.length + "\n" + cards.PrintCards());
+                    long time1 = System.currentTimeMillis();
+                    cards.QuickSort();
+                    long time2 = System.currentTimeMillis();
+                    result_textView2.setText("Quick: " + (time2-time1)/1000. + "sec\n" + cards.PrintCards());
+                } catch(Exception e){
+                }
             }
         });
-
-
-
-
-    }
-
-    public void CompareSort(int index){
-
-    }
-    public void BubbleSort(int index){
-
-    }
-    public void InsertSort(int index){
-
-    }
-    public void SelectSort(int index){
-
-    }
-    public void MergeSort(int index){
-
-    }
-    public void HeapSort(int index){
-
-    }
-    public void QuickSort(int index){
 
     }
 
--- app/src/main/java/com/example/junho/algorithm2/PackofCards.java
+++ app/src/main/java/com/example/junho/algorithm2/PackofCards.java
@@ -1,5 +1,9 @@
 package com.example.junho.algorithm2;
 
+import android.renderscript.Sampler;
+
+import java.util.Random;
+
 /**
  * Created by junho on 2017. 4. 10..
  */
@@ -25,13 +29,89 @@ public class PackofCards {
             "Queen",
             "King"
     };
+    static Random rand = new Random();
+    Card[] pack;
+    int length;
+    /*inner class of PackofCards*/
+    public class Card{
+        int Suit;
+        int Value;
 
+        Card(){
+            Suit = 0;
+            Value = 1;
+        }
+        Card(int a, int b) {
+            Suit = a;
+            Value = b + 1;
+        }
+    }
 
+    PackofCards(int size){
+        rand.setSeed(seed1);
+        pack = new Card[size];
+        for(int i = 0; i <size; i++){
+            pack[i] = new Card(rand.nextInt(4), rand.nextInt(13));
+        }
+        length = size;
+    }
 
+    PackofCards(int size, long seed){
+        rand.setSeed(seed);
+        pack = new Card[size];
+        for(int i = 0; i < size; i++){
+            pack[i] = new Card(rand.nextInt(4), rand.nextInt(13));
+        }
+        length = size;
+    }
 
+    boolean CardCompare(Card card1, Card card2){
+        if(card1.Value != card2.Value){
+            return (card1.Value > card2.Value);
+        }
+        else{
+            return (card1.Suit > card2.Suit);
+        }
+    }
 
+    String PrintCards(){
+        String print = "";
+        for(int i = 0; i<length; i++){
+            print = print.concat((i+1)+"."+ Values[pack[i].Value]+"of"+Suits[pack[i].Suit]+"\n");
+        }
+        return print;
+    }
 
+    /*Sorting Operation*/
+    void BubbleSort(){
+        int i, j;
+        Card TmpCard = new Card(0, 1);
 
+        for(i = length -1; i > 0; i--){
+            for(j = 0; j <i; j++){
+                if(CardCompare(pack[j], pack[j+1])){
+                    TmpCard = pack[j];
+                    pack[j] = pack[j+1];
+                    pack[j+1] = TmpCard;
+                }
+            }
+        }
+    }
+    void InsertSort(){
+
+    }
+    void SelectSort(){
+
+    }
+    void MergeSort(){
+
+    }
+    void HeapSort(){
+
+    }
+    void QuickSort(){
+
+    }
 
 
 
